ef0153c76d8f7e41248b7f99922066d79a313540
fzleli <amirfzlelii@gmail.com>
2023-12-11 21:16:17 +0330

    init SelfHostServer project

---
+++ src/SelfHostServer.Host/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SelfHostServer.Host
+{
+    public class ProductController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product{Id = 1, Name ="Product 1" },
+            new Product{Id = 2, Name ="Product 2" },
+            new Product{Id = 3, Name ="Product 3" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> All()
+        {
+            return products;
+        }
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
